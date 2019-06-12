e88eabd5f964c3cbb69ef4a96e5cbd56d00217f4
MIClavelTe <miclavelte@gmail.com>
2019-06-12 15:25:37 -0700

    Create an icon and a highScore feature

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,6 +2,7 @@ import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 
 import Counter from './Counter'
+import Icon from './Icon'
 
 class Player extends PureComponent {
     static propTypes = {
@@ -10,16 +11,18 @@ class Player extends PureComponent {
       name: PropTypes.string.isRequired,
       score: PropTypes.number.isRequired,
       id: PropTypes.number,
-      index: PropTypes.number
+      index: PropTypes.number,
+      isHighScore: PropTypes.bool
     }
   
     render () {
-      const {name, score, index, id, removePlayer, changeScore} = this.props
+      const {name, score, index, id, removePlayer, changeScore, isHighScore} = this.props
 
       return (
         <div className="player">
           <span className="player-name">
             <button className="remove-player" onClick={() => removePlayer(id)}>✖</button>
+            <Icon isHighScore={isHighScore}/>
             { name }
           </span>
     
